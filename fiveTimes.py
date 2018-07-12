b4399c944c1f740f5b740cbdc693911f19825900
Libby Wichman <libby.wichman@gmail.com>
2018-07-11 23:06:51 -0500

    Add files via upload

---
+++ fiveTimes.py
@@ -0,0 +1,12 @@
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times (' + str(i) + ')')
+
+# for loops are more consise, but the while loop below is another way to execute the same thing.
+
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times ('  + str(i) + ')')
+    i = i + 1
+    
